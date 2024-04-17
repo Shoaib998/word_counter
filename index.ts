d58319f6d1391ebaf8ee80cd03bd498412b92591
Muhammad Shoaib Khan <shoaib.amin998@gmail.com>
2024-04-17 23:31:27 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk"
+
+const answers: {
+    Sentence: string
+} = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your Sentence to count your words: "
+    }
+])
+
+const words = answers.Sentence.trim().split(" ")
+
+console.log(words);
+
+console.log(`Your Senetence word count is: ${chalk.blue(words.length)}`);
+
